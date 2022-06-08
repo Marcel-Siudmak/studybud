22d08e7883d1c459177600aec645b0a1db81cf5d
Marcel-Siudmak <siudmakmarcel7@gmail.com>
2022-06-08 20:18:17 +0200

    I added Idea and Votes models

---
+++ pomysly/ideas/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+IDEA_STATUS = (
+    ('pending', 'waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    youtube = models.URLField()
+    status = models.CharField(choices=IDEA_STATUS, max_length=35, default='pending')
+
+class Votes(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField
