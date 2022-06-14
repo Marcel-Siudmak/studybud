825df9eb55c8a6bb06ee0f7541ffbd331ae00164
Marcel-Siudmak <siudmakmarcel7@gmail.com>
2022-06-14 12:50:30 +0200

    admin pandel

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,4 +1,16 @@
+from turtle import update
+from venv import create
 from django.db import models
+from sqlalchemy import desc
 
+class Room(models.Model):
+    #host =
+    #topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-
+    def __str__(self) -> str:
+        return self.name
