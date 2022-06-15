5cc2d75978aea47829c77eef8c1942852efda38f
Marcel-Siudmak <siudmakmarcel7@gmail.com>
2022-06-15 15:19:37 +0200

    searchbar

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,15 +1,25 @@
+from calendar import c
 from email.mime import base
 from multiprocessing import context
 import re
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -41,3 +51,10 @@ def update_room(request, pk):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
